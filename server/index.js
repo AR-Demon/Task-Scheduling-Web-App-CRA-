1b2d9043ba2bf053944aaf751090c411b26643e9
Ar-Demon <praffulthapa11@gmail.com>
2023-04-09 14:51:44 +0545

    Set Configurations and file storage with multer

--- server/index.js
+++ server/index.js
@@ -9,6 +9,33 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { error } from "console";
-import { authRoutes } from "./routes/auth.js"
-import { register } from "./controller/auth.js"
+import authRoutes from "./routes/auth.js"
+import { register } from "./controllers/auth.js"
 import { verifyToken } from "./middleware/auth.js";
+
+/*  CONFIGURATIONS  */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname =  path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy : "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+/* FILE STORAGE */
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, "public/assets");
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage});
